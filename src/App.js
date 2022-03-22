7fa86615ea03da4928c0ec9da2ef10d636b49092
Nguyen <tranchauthanhnguyen@gmail.com>
2022-03-23 00:32:27 +0700

    buoi2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import ClassComponent from './Component/ClassComponent';
+import FunctionComponent from './Component/FunctionComponent';
+import HomeComponent from './Component/BTComponent/HomeComponent';
+import BindingData from './BindingData/BindingData';
+import HandleEvent from './HandleEvent/HandleEvent';
 
 // các thẻ html của react js là đối tương JSX
+//App la component chinh cua du an
 function App() {
   return (
     <div className="App">
-     hello dn02
-      
+     {/* hello dn02
      
+      <ClassComponent/>
+      <ClassComponent/>
+      <FunctionComponent/> */}
+
+      {/* <HomeComponent/> */}
+
+      {/* <BindingData/> */}
+      {/* <p className='colorPrice'>Lorem ipsum dolor sit amet.</p> */}
+   
+      <HandleEvent/>
+   
     </div>
   );
 }
