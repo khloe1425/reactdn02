25dc01e7c11d85e6d6b44869fc80a3f6888bc3e0
Nguyen <tranchauthanhnguyen@gmail.com>
2022-03-24 19:07:29 +0700

    update

--- src/HandleEvent/HandleEvent.js
+++ src/HandleEvent/HandleEvent.js
@@ -2,13 +2,30 @@ import React, { Component } from 'react'
 
 export default class HandleEvent extends Component {
 
+    name = "Nguyen Thi Hoc Vien";
+
+    renderName = () => {
+        alert("Ten HV:"+ this.name);
+    }
+
+    loadName = () => {
+        alert("Ten HV khi load web:"+ this.name);
+    }
     render() {
+        let age = 20;
+
         return (
-            <div>HandleEvent
+            <div>
+                HandleEvent
+                {/* Chay ngay khi load web */}
+                <p>{this.loadName()}</p>
 
                 <button onClick={() => {
                     alert("Click");
                 }} >Click</button>
+
+                {/* chi chay khi nguoi dung click */}
+                <button onClick={this.renderName}>Show Name</button>
             </div>
         )
     }
